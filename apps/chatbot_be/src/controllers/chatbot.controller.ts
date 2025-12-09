8d29d490c46108708ffafa8ea974572f109cf1c7
KhanhDo2604 <20521442@gm.uit.edu.vn>
2025-12-10 09:56:14 +1300

    progress 1 chatbot logic

--- apps/chatbot_be/src/controllers/chatbot.controller.ts
+++ apps/chatbot_be/src/controllers/chatbot.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 
-export const getChatbotStatusController = (
+export const handleQuestionController = (
   request: Request,
   response: Response
 ) => {
   try {
+    const { question } = request.body;
   } catch (error: Error | any) {
     return { status: 500, message: error.message };
   }
